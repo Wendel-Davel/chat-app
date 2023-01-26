6d0c1169e952dc14fc25da1d347eebc7ec6d9ef6
Wendel Davel <81473773+Wendel-Davel@users.noreply.github.com>
2023-01-26 11:15:08 -0300

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,12 +4,7 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZj_AtEht0rLPs0JB6xSgcYzOair1RWdY",
-  authDomain: "app-chat-588f6.firebaseapp.com",
-  projectId: "app-chat-588f6",
-  storageBucket: "app-chat-588f6.appspot.com",
-  messagingSenderId: "976370179392",
-  appId: "1:976370179392:web:937004fb81717af48eaea5"
+
 };
 
 const app = firebase.initializeApp(firebaseConfig);
